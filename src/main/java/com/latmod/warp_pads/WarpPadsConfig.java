b5710285e6ece7d2eaad959714525ef97c3d20f9
LatvianModder <latvianmodder@gmail.com>
2017-08-18 03:23:43 +0300

    Removed registries, everything now is Event-based

--- src/main/java/com/latmod/warp_pads/WarpPadsConfig.java
+++ src/main/java/com/latmod/warp_pads/WarpPadsConfig.java
@@ -1,23 +1,27 @@
 package com.latmod.warp_pads;
 
-import com.feed_the_beast.ftbl.api.IFTBLibRegistry;
+import com.feed_the_beast.ftbl.api.EventHandler;
+import com.feed_the_beast.ftbl.api.events.registry.RegisterConfigEvent;
 import com.feed_the_beast.ftbl.lib.config.PropertyDouble;
 import com.feed_the_beast.ftbl.lib.config.PropertyInt;
 import com.feed_the_beast.ftbl.lib.util.CommonUtils;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.File;
 
+@EventHandler
 public class WarpPadsConfig // WarpPads
 {
 	private static final PropertyDouble LEVELS_PER_BLOCK = new PropertyDouble(0.001D, 0D, 5D);
 	private static final PropertyInt LEVELS_FOR_CROSSDIM = new PropertyInt(30, 0, 200);
 
-	public static void init(IFTBLibRegistry reg)
+	@SubscribeEvent
+	public static void init(RegisterConfigEvent event)
 	{
-		reg.addConfigFileProvider(WarpPads.MOD_ID, () -> new File(CommonUtils.folderConfig, "WarpPads.json"));
+		event.registerFile(WarpPads.MOD_ID, () -> new File(CommonUtils.folderConfig, "WarpPads.json"));
 
-		reg.addConfig(WarpPads.MOD_ID, "general.levels_per_block", LEVELS_PER_BLOCK);
-		reg.addConfig(WarpPads.MOD_ID, "general.levels_for_crossdim", LEVELS_FOR_CROSSDIM);
+		event.register(WarpPads.MOD_ID, "general.levels_per_block", LEVELS_PER_BLOCK);
+		event.register(WarpPads.MOD_ID, "general.levels_for_crossdim", LEVELS_FOR_CROSSDIM);
 	}
 
 	public static int getEnergyRequired(double distance)
--- src/main/java/com/latmod/warp_pads/WarpPadsEventHandler.java
+++ src/main/java/com/latmod/warp_pads/WarpPadsEventHandler.java
@@ -1,8 +1,6 @@
 package com.latmod.warp_pads;
 
 import com.feed_the_beast.ftbl.api.EventHandler;
-import com.feed_the_beast.ftbl.api.events.FTBLibRegistryEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * @author LatvianModder
@@ -10,9 +8,4 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @EventHandler
 public class WarpPadsEventHandler
 {
-	@SubscribeEvent
-	public static void registerCommon(FTBLibRegistryEvent event)
-	{
-		WarpPadsConfig.init(event.getRegistry());
-	}
 }
