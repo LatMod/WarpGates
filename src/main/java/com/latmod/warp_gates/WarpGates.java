3f2d9091b2a7d8fd6dda84f13698e54d26d3d781
LatvianModder <latvianmodder@gmail.com>
2017-09-27 02:05:44 +0300

    Added gradle.properties, updated forge & mappings

--- src/main/java/com/latmod/warp_gates/WarpGates.java
+++ src/main/java/com/latmod/warp_gates/WarpGates.java
@@ -1,16 +1,17 @@
 package com.latmod.warp_gates;
 
 import com.latmod.warp_gates.block.WarpGateNet;
-import com.latmod.warp_gates.net.WarpGatesNetHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 
-@Mod(modid = WarpGates.MOD_ID, name = "Warp Gates", version = "@VERSION@", acceptedMinecraftVersions = "[1.12,)", dependencies = "required-after:ftbl")
+@Mod(modid = WarpGates.MOD_ID, name = WarpGates.MOD_NAME, version = WarpGates.VERSION, acceptedMinecraftVersions = "[1.12,)", dependencies = "required-after:ftbl")
 public class WarpGates
 {
 	public static final String MOD_ID = "warp_gates";
+	public static final String MOD_NAME = "Warp Gates";
+	public static final String VERSION = "@VERSION@";
 
 	@Mod.Instance(WarpGates.MOD_ID)
 	public static WarpGates INST;
@@ -21,8 +22,7 @@ public class WarpGates
 	@Mod.EventHandler
 	public void onPreInit(FMLPreInitializationEvent event)
 	{
-		WarpGatesConfig.sync();
-		WarpGatesNetHandler.init();
+		PROXY.preInit();
 	}
 
 	@Mod.EventHandler
--- src/main/java/com/latmod/warp_gates/WarpGatesCommon.java
+++ src/main/java/com/latmod/warp_gates/WarpGatesCommon.java
@@ -1,8 +1,15 @@
 package com.latmod.warp_gates;
 
+import com.latmod.warp_gates.net.WarpGatesNetHandler;
+
 /**
  * @author LatvianModder
  */
 public class WarpGatesCommon
 {
+	public void preInit()
+	{
+		WarpGatesConfig.sync();
+		WarpGatesNetHandler.init();
+	}
 }
